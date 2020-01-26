ad5767f8288fe182189c5a8d01374724600e9aff
decoder <piotrzan@gmail.com>
2020-01-26 16:17:33 +0100

    changed AKS provisioning script to work with subscription

--- Exercises/kubernetes-services/azure-cli/create.sh
+++ Exercises/kubernetes-services/azure-cli/create.sh
@@ -1,6 +1,8 @@
-# Global variables
-export group=aks-lab
-export region=westeurope
+#!/bin/bash
+
+# Setup variables
+group=aks-lab
+region=westeurope
 
 # Local variables
 NODE_COUNT=3
@@ -8,10 +10,19 @@ NODE_COUNT=3
 # Create resource group
 az group create -n $group -l $region
 
-# Create AKS cluster
+# Get latest version of k8s for your region
+version=$(az aks get-versions -l "$region" --query 'orchestrators[-1].orchestratorVersion' -o tsv)
+
+# Create AKS cluster on your subscription, without using service principal
 az aks create -g $group -n aks-cluster -l $region \
---node-count $NODE_COUNT --node-vm-size Standard_DS2_v2 \
---kubernetes-version 1.14.8 --verbose
+    --node-count $NODE_COUNT \
+    --node-vm-size Standard_DS2_v2 \
+    --generate-ssh-keys \
+    --vm-set-type VirtualMachineScaleSets \
+    --enable-cluster-autoscaler \
+    --min-count 1 \
+    --max-count 3
+    --kubernetes-version "$version" --verbose
 
 # Set cluster in kubeconfig
 az aks get-credentials -g $group -n aks-cluster
