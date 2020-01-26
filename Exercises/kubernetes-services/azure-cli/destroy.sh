e7bb8e32fd0a5bf4c53d2b1d3363f1e9f9cc8279
decoder <piotrzan@gmail.com>
2020-01-26 17:55:54 +0100

    setup

--- Exercises/kubernetes-services/azure-cli/destroy.sh
+++ Exercises/kubernetes-services/azure-cli/destroy.sh
@@ -9,10 +9,10 @@ region=westeurope
 # Local variables
 CLUSTER_RESOURCES_GROUP='MC_'$group'_aks-cluster_'$region
 
-# Delete aks vms and other support resources
-echo 'Removing support group '$CLUSTER_RESOURCES_GROUP
-az group delete --name $CLUSTER_RESOURCES_GROUP --yes
-
 # Delete aks resource group and cluster itself
 echo 'Removing main group with the cluster '$group
 az group delete --name $group --yes
+
+# Delete aks vms and other support resources
+echo 'Removing support group '$CLUSTER_RESOURCES_GROUP
+az group delete --name $CLUSTER_RESOURCES_GROUP --yes
