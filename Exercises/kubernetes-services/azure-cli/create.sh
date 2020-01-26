3d65835c53b16317b47f1db8aa71fb5e7a7dcce4
decoder <piotrzan@gmail.com>
2020-01-26 16:54:31 +0100

    adde steps to script

--- Exercises/kubernetes-services/azure-cli/create.sh
+++ Exercises/kubernetes-services/azure-cli/create.sh
@@ -8,14 +8,14 @@ region=westeurope
 NODE_COUNT=3
 
 # Create resource group
-echo 'Creating resource group '$group 'in region '$region
+echo '[STEP 1] Creating resource group '$group 'in region '$region
 az group create -n $group -l $region
 
 # Get latest version of k8s for your region
 version=$(az aks get-versions -l "$region" --query 'orchestrators[-1].orchestratorVersion' -o tsv)
 
 # Create AKS cluster on your subscription, without using service principal and with autoscaler
-echo 'Spinning up AKS cluster aks-cluster in group '$group' in region '$region' using '$version' of kubernetes'
+echo '[STEP 2] Spinning up AKS cluster aks-cluster in group '$group' in region '$region' using '$version' of kubernetes'
 az aks create -g $group -n aks-cluster -l $region \
     --node-count $NODE_COUNT \
     --node-vm-size Standard_DS2_v2 \
@@ -28,11 +28,11 @@ az aks create -g $group -n aks-cluster -l $region \
     --kubernetes-version "$version" --verbose
 
 # Set cluster in kubeconfig
-echo 'Setting up connection to cluster for Azure Shell'
+echo '[STEP 3] Setting up connection to cluster for Azure Shell'
 az aks get-credentials -g $group -n aks-cluster
 
 # Get some aliases
-echo 'Creating useful aliases'
+echo '[STEP 4] Creating useful aliases'
 
 # Instead of typing kubectl all the time, abbreviate it to just “k”
 alias k=kubectl
