6910eb07c72c26b82cb00e6b821895198c0dd51f
decoder <piotrzan@gmail.com>
2020-01-26 17:27:52 +0100

    verbose parameters for destroy script

--- Exercises/kubernetes-services/azure-cli/destroy.sh
+++ Exercises/kubernetes-services/azure-cli/destroy.sh
@@ -11,8 +11,8 @@ CLUSTER_RESOURCES_GROUP='MC_'$group'_aks-cluster_'$region
 
 # Delete aks vms and other support resources
 echo 'Removing support group '$CLUSTER_RESOURCES_GROUP
-az group delete -n $CLUSTER_RESOURCES_GROUP -y
+az group delete --name $CLUSTER_RESOURCES_GROUP --yes
 
 # Delete aks resource group and cluster itself
 echo 'Removing main group with the cluster '$group
-az group delete -n $group -y
+az group delete --name $group --yes
